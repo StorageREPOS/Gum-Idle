4c23ad42e0b741bf2f9d0268c415f158c5af549e
StoreageREPOS <NateLandis@protonmail.com>
2024-11-08 23:01:07 +0000

    make code smoother

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ursina import *
 app = Ursina()
 #Set Vars
 window.color = color.gray
-gold = 0
+gum = 0
 extended = True
 #Set Entities
 upanel = Entity(model='quad', scale=(3, 200), color=color.dark_gray, alpha=0.5, x=-5.8)
@@ -14,24 +14,20 @@ counter = Text(text='0', y=.25, z=-1, scale=2, origin=(0, 0), background=True)
 #Set Buttons
 collapser = Button(texture=load_texture("collapser.png"), color=color.light_gray, scale=.125, z=-3, x=-.6, y=.45)
 gumball = Button(texture=load_texture("gumball_pink.png"), color=color.white, scale=.225)
-gold_gen_upgrade = Button(text="Gold Gen",cost=10, x=-.71, scale_y =.125, scale_x=.250, color=color.red,y=.33, disabled=True)
-gold_gen_upgrade.tooltip = Tooltip(f'<gold>Gold Generator\n<default>Earn 1 gold every second.\nCosts {gold_gen_upgrade.cost} gold.')
+gum_up = Button(text="Gum Gen",cost=10, x=-.71, scale_y =.125, scale_x=.250, color=color.red,y=.33, disabled=True)
+gum_up.tooltip = Tooltip(f'<pink>Gum Generator\n<default>Earn 1 gum every second.\nCosts {gum_up.cost} gum.')
 #TLD Methods
 def mov_ups():
     for outer_key, outer_value in upgrades.upgrades.items():
       if isinstance(outer_value, dict):
             for inner_key, inner_value in outer_value.items():
                 if inner_key == "name":
-                    print(f"Found 'name' under key '{outer_key}': {inner_value}")
                     mod = globals().get(inner_value,"No such variable")
-                    print(mod)
-                    print(outer_key)
                     cx=0
                     cy=0
                     ey=0
                     ex=0
                     for ik, iv in outer_value.items():
-                        print(iv)
                         match ik:
                             case "cx":
                                 cx = iv
@@ -41,7 +37,6 @@ def mov_ups():
                                 ey=iv
                             case "ex":
                                 ex=iv
-                    print(f"ex:{ex}ey:{ey}cx:{cx}cy:{cy}")
                     if extended == True:
                         invoke(mod.animate('x',ex,duration=1,curve=curve.out_bounce))
                         invoke(mod.animate('y',ey,duration=1,curve=curve.out_bounce))
@@ -50,17 +45,17 @@ def mov_ups():
                         invoke(mod.animate('y',cy,duration=1,curve=curve.out_bounce))
 #Signals
 def gumball_click():
-    global gold
-    gold += 1
-    counter.text = str(gold)
+    global gum
+    gum += 1
+    counter.text = str(gum)
     gumball.animate_scale(.240, .2, curve=curve.linear)
     invoke(gumball.animate_scale, 0.225, duration=0.2, curve=curve.out_bounce, delay=0.2)
-def buy_auto_gold():
-    global gold
-    if gold >= gold_gen_upgrade.cost:
-        gold -= gold_gen_upgrade.cost
-        counter.text = str(gold)
-        invoke(auto_generate_gold, 1, 1)
+def buy_auto_gum():
+    global gum
+    if gum >= gum_up.cost:
+        gum -= gum_up.cost
+        counter.text = str(gum)
+        invoke(auto_generate_gum, 1, 1)
 def collapse():
     global extended
     if extended:
@@ -77,20 +72,19 @@ def collapse():
         mov_ups()
 #Signal Connections
 gumball.on_click = gumball_click
-gold_gen_upgrade.on_click = buy_auto_gold
+gum_up.on_click = buy_auto_gum
 collapser.on_click = collapse
 #Methods
-def auto_generate_gold(value=1, interval=1):
-    global gold
-    gold += value
-    counter.text = str(gold)
-    invoke(auto_generate_gold, value, delay=interval)
+def auto_generate_gum(value=1, interval=1):
+    global gum
+    gum += value
+    counter.text = str(gum)
+    invoke(auto_generate_gum, value, delay=interval)
 #Game Loop
 def update():
-    global gold
-    print(gold_gen_upgrade.position)
-    for b in (gold_gen_upgrade, ):
-        if gold >= b.cost:
+    global gum
+    for b in (gum_up, ):
+        if gum >= b.cost:
             b.disabled = False
             b.color = color.green
         else:
--- upgrades.py
+++ upgrades.py
@@ -1,6 +1,6 @@
 upgrades = {
-    "gold_gen":{
-        "name": "gold_gen_upgrade",
+    "gum_gen":{
+        "name": "gum_up",
         "ex": -.71,
         "ey":.33,
         "cx":-1.1,
